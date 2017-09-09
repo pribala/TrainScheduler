133962553f1efeae7c5d7b8a792315328f4babae
Priya Balakrishnan <andu_pri@yahoo.com>
2017-09-09 18:00:48 -0400

    fix: Update google sign in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -98,4 +98,28 @@ $(document).ready(function () {
             firebase.auth().signInWithRedirect(provider);
         });
 
+        firebase.auth().getRedirectResult().then(function(result) {
+          if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // ...
+            $("#signIn").text("Signout");
+          }
+          // The signed-in user info.
+          var user = result.user;
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          console.log(errorCode);
+          var errorMessage = error.message;
+          console.log(errorMessage);
+          // The email of the user's account used.
+          var email = error.email;
+          console.log(email);
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          console.log(credential);
+          // ...
+        });
+
     });
