ceb091083a505970fd2366cedb9ac563c205a2c6
Priya Balakrishnan <andu_pri@yahoo.com>
2017-09-11 11:33:53 -0400

    docs: Update comments

---
+++ property_detailed.php
@@ -0,0 +1,21 @@
+<div class="col-lg-8 col-xs-12" id="right-pane">
+	<div class="card">
+        <div class="card-header">
+            <strong>Current Train Schedule</strong>
+        </div>
+    	<div class="card-body">
+        	<table class="table">
+            	<thead>
+	            	<tr>
+		                <td>Train Name</td>
+		                <td>Destination</td>
+		                <td>Frequency (min)</td>
+		                <td>Next Arrival</td>
+			            <td>Minutes Away</td>
+	            	</tr>
+	        	</thead>
+	        	<tbody id="tableBody"></tbody>
+	   		</table>
+    	</div>
+    </div>
+</div>
